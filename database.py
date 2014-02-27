379d0f1ba4b965bef0f7510a503f92e3393d3118
Katerina Hanson <katerina.hanson@gmail.com>
2014-02-26 16:03:08 -0800

    Initial commit

---
+++ database.py
@@ -0,0 +1,164 @@
+import sqlite3
+
+
+class Database():
+    def __init__(self, path=None):
+        if path is None:
+            self.path = 'database.sqlite'
+        else:
+            self.path = path
+
+    def _create_connection(self):
+        self.connection = sqlite3.connect(self.path)
+        self._cursor = self.connection.cursor()
+
+    def save(self):
+        self.connection.commit()
+
+    def _close_connection(self):
+        self.connection.close()
+        self.connection = None
+        self._cursor = None
+
+    def create_tables(self, tabledata):
+        ''' Tabledata in form {Tablename: {column_name: type}}
+        '''
+        self._create_connection()
+        for tablename, atts in tabledata.iteritems():
+            start = "CREATE TABLE %s (id INTEGER PRIMARY KEY, " % tablename.lower()
+            temp = []
+            for item in atts:
+                mytype = atts[item].upper()
+                temp.append("%s %s" % (item, mytype))
+            tempstr = ", ".join(temp)
+            command = start + tempstr + (");")
+            #print command
+            self._cursor.execute(command)
+        self.save()
+        self._close_connection()
+        return True
+
+    def get_table_list(self):
+        self._create_connection()
+        cursor = self._cursor.execute(
+            "SELECT name FROM sqlite_master WHERE type='table' ORDER BY name;")
+        tabledata = cursor.fetchall()
+        self._close_connection()
+        tablelist = []
+        for item in tabledata:
+            tablelist.append(item[0])
+        return tablelist
+
+    def get_columns(self, tablename):
+        self._create_connection()
+        command = "PRAGMA table_info( %s );" % tablename.lower()
+        cursor = self._cursor.execute(command)
+        columns = cursor.fetchall()
+        self._close_connection()
+        colnames = []
+        for col in columns:
+            colnames.append(col[1])
+        return colnames
+
+    def delete(self, tablename, myid):
+        self._create_connection()
+        command = "DELETE FROM %s WHERE id == ?;" % tablename.lower()
+        self._cursor.execute(command, (myid,))
+        self.save()
+        self._close_connection()
+        return True
+
+    def update(self, tablename, row, dic):
+        self._create_connection()
+        for key, value in dic.iteritems():
+            command = "UPDATE %s SET %s = ? WHERE id = ?;" % (tablename.lower(), key)
+            self._cursor.execute(command, (value, row))
+        self.save()
+        self._close_connection()
+        return True
+
+    def add(self, tablename, dic):
+        key = dic.keys()[0]
+        command = "INSERT INTO %s (%s) VALUES (?);" % (tablename.lower(), key)
+        value = dic[key]
+        command2 = "SELECT last_insert_rowid();"
+        self._create_connection()
+        cursor = self._cursor.execute(command, (value,))
+        cursor = self._cursor.execute(command2)
+        myid = cursor.fetchone()
+        self.save()
+        self._close_connection()
+        self.update(tablename, myid[0], dic)
+        return myid[0]
+
+    def _query_table_all(self, tablename):
+        columns = self.get_columns(tablename)
+        values = self._query_cols_all(tablename, columns)
+        answerlist = []
+        for row in values:
+            answerdict = {}
+            answerlist.append(answerdict)
+            i = 0
+            for col in columns:
+                answerdict[col] = row[i]
+                i += 1
+        return answerlist
+
+    def _query_table_row(self, tablename, row):
+        columns = self.get_columns(tablename)
+        answerdict = {}
+        self._create_connection()
+        for col in columns:
+            command = "SELECT %s FROM %s WHERE id == ?;" % (col, tablename.lower())
+            cursor = self._cursor.execute(command, (row,))
+            answer = cursor.fetchone()
+            answerdict[col] = answer[0]
+        self._close_connection()
+        return answerdict
+
+    def _query_cols_all(self, tablename, colist):
+        columns = ', '.join(colist)
+        command = "SELECT %s FROM %s;" % (columns, tablename.lower())
+        self._create_connection()
+        cursor = self._cursor.execute(command)
+        answerlist = cursor.fetchall()
+        self._close_connection()
+        return answerlist
+
+    def _query_cols_where(self, tablename, colist, wheretup):
+        columns = ', '.join(colist)
+        command = "SELECT %s FROM %s WHERE %s = ?;" % (columns, tablename.lower(), wheretup[0])
+        self._create_connection()
+        cursor = self._cursor.execute(command, (wheretup[1],))
+        answerlist = cursor.fetchall()
+        self._close_connection()
+        return answerlist
+
+    def _query_where(self, tablename, wheretup):
+        command = "SELECT id FROM %s WHERE %s = ?;" % (tablename.lower(), wheretup[0])
+        self._create_connection()
+        cursor = self._cursor.execute(command, (wheretup[1],))
+        rows = cursor.fetchall()
+        self._close_connection()
+        anslist = []
+        for row in rows:
+            anslist.append(self._query_table_row(tablename, row[0]))
+        return anslist
+
+    def query(self, tablename, col=False, cols=False, row=False, wcol=False, wval=False):
+        if wcol is not False and wval is not False:
+            if cols:
+                answers = self._query_cols_where(tablename, cols, (wcol, wval))
+            elif col:
+                answers = self._query_cols_where(tablename, [col], (wcol, wval))
+            else:
+                answers = self._query_where(tablename, (wcol, wval))
+        elif row:
+            answers = self._query_table_row(tablename, row)
+        elif cols:
+            answers = self._query_cols_all(tablename, cols)
+        elif col:
+            answers = self._query_cols_all(tablename, [col])
+        else:
+            answers = self._query_table_all(tablename)
+        return answers
---
+++ test.py
@@ -0,0 +1,89 @@
+from database import Database
+import unittest
+import os
+
+tables = {'tables': {'legs': 'integer', 'material': 'text'},
+          'clients': {'name': 'text', 'address': 'text', 'phone': 'integer'}}
+
+
+class DbTest(unittest.TestCase):
+
+    def setUp(self):
+        self.db = Database()
+        self.db.create_tables(tables)
+        self.tablename = 'tables'
+
+    def tearDown(self):
+        os.remove('database.sqlite')
+
+    def test_exists(self):
+        test = os.path.isfile('database.sqlite')
+        self.assertEqual(test, True)
+
+    def test_add(self):
+        row = self.db.add(self.tablename, {'legs': 4, 'material': 'wood'})
+        self.assertEqual(row, 1)
+
+        test = self.db.delete(self.tablename, row)
+        self.assertEqual(test, True)
+
+    def test_tables(self):
+        table_list = self.db.get_table_list()
+        self.assertIn(self.tablename, table_list)
+
+    def test_query(self):
+        row = self.db.add(self.tablename, {'legs': 4, 'material': 'wood'})
+        self.assertEqual(row, 1)
+
+        result = self.db.query(self.tablename)[0]
+        self.assertEqual(result['legs'], 4)
+        self.assertEqual(result['material'], 'wood')
+
+        test = self.db.delete(self.tablename, row)
+        self.assertEqual(test, True)
+
+    def test_query_table_row(self):
+        row = self.db.add('clients', {'name': 'Fischer', 'address': 'Next door', 'phone': 23645})
+        self.assertEqual(row, 1)
+        row = self.db.add('clients', {'name': 'Tolouse', 'address': '55th st', 'phone': 8928384})
+        self.assertEqual(row, 2)
+
+        result = self.db.query('clients', row=2)
+        self.assertEqual(result['id'], 2)
+        self.assertEqual(result['name'], 'Tolouse')
+
+    def test_query_cols_all(self):
+        row = self.db.add('clients', {'name': 'Fischer', 'address': 'Next door', 'phone': 23645})
+        self.assertEqual(row, 1)
+        row = self.db.add('clients', {'name': 'Tolouse', 'address': '55th st', 'phone': 8928384})
+        self.assertEqual(row, 2)
+
+        result = self.db.query('clients', cols=['id', 'name'])
+
+        index = 1
+        for row in result:
+            self.assertEqual(row[0], index)
+            index += 1
+
+    def test_query_cols_where(self):
+        row = self.db.add('clients', {'name': 'Fischer', 'address': 'Next door', 'phone': 23645})
+        self.assertEqual(row, 1)
+        row = self.db.add('clients', {'name': 'Tolouse', 'address': '55th st', 'phone': 8928384})
+        self.assertEqual(row, 2)
+
+        result = self.db.query('clients', cols=['id', 'phone'], wcol='phone', wval=23645)
+        result = result[0]
+        self.assertEqual(result[0], 1)
+
+    def test_query_where(self):
+        row = self.db.add('clients', {'name': 'Fischer', 'address': 'Next door', 'phone': 23645})
+        self.assertEqual(row, 1)
+        row = self.db.add('clients', {'name': 'Tolouse', 'address': '55th st', 'phone': 8928384})
+        self.assertEqual(row, 2)
+
+        result = self.db.query('clients', wcol='phone', wval=23645)
+        result = result[0]
+        self.assertEqual(result['id'], 1)
+
+if __name__ == '__main__':
+    unittest.main()
